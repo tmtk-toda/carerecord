4dee41a679dc485493e5ab513d20c37b689037cc
tmtk <blindhurts.6001@gmail.com>
2020-09-08 03:46:30 +0900

    devise追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'clients#index'
   get 'clients/index'
   get '/clients', to: 'clients#index'
   get 'care_records/index'
   get '/care_records', to: 'care_records#index'
   resources :care_records
   resources :clients
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
