869be69346dccb724419b60e6dfc26549207ba1f
tmtk <blindhurts.6001@gmail.com>
2020-09-09 07:00:32 +0900

    微修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   get '/clients', to: 'clients#index'
   get 'care_records/index'
   get '/care_records', to: 'care_records#index'
-  resources :clients do
-    resources :care_records
-  end
+  resources :care_records
+  resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
