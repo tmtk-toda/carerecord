f04fcfa41609914de5eb991504d2675ce022f5d6
tmtk <blindhurts.6001@gmail.com>
2020-09-09 21:27:33 +0900

    devese修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get '/care_records', to: 'care_records#index'
   resources :care_records
   resources :clients
+  root 'clients#index'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
