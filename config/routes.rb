af622f14f13cd4a41fe1387db02b205a3b8ddbe8
tmtk <blindhurts.6001@gmail.com>
2020-10-09 14:26:53 +0900

    メーる変更

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,6 @@ Rails.application.routes.draw do
   end
   resources :clients
   root 'users#index'
-  if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
